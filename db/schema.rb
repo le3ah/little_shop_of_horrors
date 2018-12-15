4b96fb8fa374cab44467b6526a9319c2e2322c4d
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-15 16:17:08 -0700

    adds migration for creating orders

---
+++ db/schema.rb
@@ -0,0 +1,37 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20181215231332) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "status"
+    t.bigint "users_id"
+    t.index ["users_id"], name: "index_orders_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role"
+    t.boolean "enabled"
+    t.string "address"
+    t.string "city"
+    t.integer "zip"
+    t.string "state"
+  end
+
+  add_foreign_key "orders", "users", column: "users_id"
+end

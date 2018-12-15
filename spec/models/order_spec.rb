d48e90befdd2825092744359183afb44db869d3a
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-15 15:43:00 -0700

    adds order_spec. testing for validation and relationship

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe Order, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :status }
+  end
+
+  describe 'Relationships' do
+    it { should belong_to(:user) }
+  end
+end

702a8d78bc992c4fdb78f6434aeb47209f08f1f2
Nico <nicovigil1@gmail.com>
2018-12-15 16:23:20 -0700

    Adds validations and uniqueness in User model

--- db/migrate/20181215221856_create_users.rb
+++ db/migrate/20181215221856_create_users.rb
@@ -4,12 +4,14 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :email
       t.string :password_digest
-      t.integer :role
+      t.integer :role, default: 0
       t.boolean :enabled
       t.string :address
       t.string :city
       t.integer :zip
       t.string :state
+
+      t.timestamps
     end
   end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  context "Validations" do
+    context "relationships" do
+        xit { should have_many(:orders) } 
+        xit { should have_many(:items) } 
+    end
+    
+    describe 'should require all fields' do 
+        it { should validate_presence_of(:name) }
+        it { should validate_presence_of(:email) }
+        it { should validate_presence_of(:password_digest) }
+        it { should validate_presence_of(:role) }
+        it { should validate_presence_of(:enabled) }
+        it { should validate_presence_of(:address) }
+        it { should validate_presence_of(:city) }
+        it { should validate_presence_of(:zip) }
+        it { should validate_presence_of(:state) }
+    end
+    
+    describe 'should require uniqueness of' do    
+        it { should validate_uniqueness_of(:email) }
+    end
+    
+  end
+
+end

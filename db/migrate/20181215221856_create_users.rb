c94dd7e05e1439e4ce4aa9000face4d75c794b5f
Nico <nicovigil1@gmail.com>
2018-12-15 15:21:39 -0700

    adds migration for User & changes test for nav

---
+++ db/migrate/20181215221856_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.integer :role
+      t.boolean :enabled
+      t.string :address
+      t.string :city
+      t.integer :zip
+      t.string :state
+    end
+  end
+end
--- spec/features/users/visitors/nav_spec.rb
+++ spec/features/users/visitors/nav_spec.rb
@@ -7,24 +7,14 @@ describe 'navigation' do
   it "should link to home page" do
     click_link "home"
     expect(current_path).to eq(root_path)
-  end
-  it "should link to items index page" do
     click_link "browse items"
     expect(current_path).to eq(items_path)
-  end
-  it "should link to merchant index page" do
     click_link "browse merchants"
     expect(current_path).to eq(merchants_path)
-  end
-  it "should link to shopping cart page" do
     click_link "shopping cart"
     expect(current_path).to eq('/cart')
-  end
-  it "should link to login page" do
     click_link "login"
     expect(current_path).to eq('/login')
-  end
-  it "should link to registration page" do
     click_link "user registration"
     expect(current_path).to eq('/register')
   end

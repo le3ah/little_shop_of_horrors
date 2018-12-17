1544a03fc851c0a70e6e84e8fa2a36242e2c772b
Leah Miller <lkmiller.87@gmail.com>
2018-12-17 16:50:57 -0700

    Adds updates to register link

--- spec/features/users/visitors/user_can_login.rb
+++ spec/features/users/visitors/user_can_login.rb
@@ -1,5 +1,21 @@
 require "rails_helper"
 
-describe '' do
-  
+describe 'as a registered user' do
+  it "should login" do
+    user = User.create(email: "tony@gmail.com", password: "test")
+
+    visit root_path
+    click_on "log in"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "email", with: user.email
+    fill_in "password", with: user.password
+
+    click_on "log in"
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_content("Welcome, #{user.name}")
+    expect(page).to have_content("log out")
+  end
 end

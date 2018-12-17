cd0935723f82ca379fdb6ff88cda28e79468f2b9
Leah Miller <lkmiller.87@gmail.com>
2018-12-17 15:27:51 -0700

    Adds update to form feature test

--- spec/features/users/visitors/user_can_register_spec.rb
+++ spec/features/users/visitors/user_can_register_spec.rb
@@ -2,15 +2,26 @@ require 'rails_helper'
 
 describe 'as a visitor' do
   it "can register a new user" do
-    username = "Anthony"
+    name = "Anthony"
+    email = "Tony@gmail.com"
+    address = "1609 Rose St. NW"
+    city = "Cullman"
+    state = "AL"
+    zip = 35055
 
     visit root_path
 
     click_on 'Sign Up'
     expect(current_path).to eq('/register')
 
-    fill_in :user_username, with: username
+    fill_in :user_name, with: name
+    fill_in :user_email, with: email
     fill_in :user_password, with: "test"
+    fill_in :user_confirm_password, with: "test"
+    fill_in :user_address, with: address
+    fill_in :user_city, with: city
+    fill_in :user_state, with: state
+    fill_in :user_zip, with: zip
 
     click_on 'Create User'
 

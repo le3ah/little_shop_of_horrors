0bad729283952cd6860b8783571d257d67115a26
Leah Miller <lkmiller.87@gmail.com>
2018-12-17 16:02:50 -0700

    Adds session functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:id] = @user.id
+      redirect_to profile_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(session[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :email, :address, :city, :state, :zip)
+  end
 end

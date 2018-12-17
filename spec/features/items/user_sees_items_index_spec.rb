f5bcf96a2d7040f8cac741fa4d3b5350f1d93a9b
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-17 16:51:07 -0700

    adds setup and testing of item info on page

---
+++ spec/features/items/user_sees_items_index_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+describe 'Items Index Page' do
+  context 'as any kind of user' do
+    before :each do
+      m_1 = Merchant.create(
+        name: "Bob's Burgers",
+        email: "email@email.com",
+        password: "1234password",
+        role: 1,
+        address: "12 Main st",
+        city: "Town",
+        state: "CO"
+      )
+
+      i_1 = m_1.items.create(
+        name: 'Burger',
+        description: 'Juicy',
+        thumbnail: 'thumbnail',
+        price: 4,
+        inventory: 5
+      )
+
+      i_2 = m_1.items.create(
+        name: 'Burger sauce',
+        description: 'Juicy sauce',
+        thumbnail: 'thumbnail',
+        price: 2,
+        inventory: 12,
+        enabled: false
+      )
+    end
+
+    it 'should show enabled item information' do
+      expect(page).to have_content(i_1.name)
+      expect(page).to have_content(i_1.description)
+      expect(page).to have_content(i_1.thumbnail)
+      expect(page).to have_content(i_1.price)
+      expect(page).to have_content(i_1.inventory)
+    end
+
+    it 'should not show disabled item information' do
+      expect(page).to_not have_content(i_2.name)
+      expect(page).to_not have_content(i_2.description)
+      expect(page).to_not have_content(i_2.thumbnail)
+      expect(page).to_not have_content(i_2.price)
+      expect(page).to_not have_content(i_2.inventory)
+    end
+  end
+end

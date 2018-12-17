86ad66f02d17b438a01152ce0b4b583f85bb8603
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-17 15:59:12 -0700

    adds routes for registration

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
-  get '/register', to: 'sessions#new'
-  post '/register', to: 'sessions#create'
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
 
   get '/cart', to: 'carts#index'
 end

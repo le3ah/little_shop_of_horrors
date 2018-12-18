033b5bfdf46146e2b6f718869201c3ca1cd83a7c
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-18 14:39:29 -0700

    adds validating enabled is either true or false

--- app/models/Item.rb
+++ app/models/Item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
-  validates_presence_of :name, :description, :thumbnail, :price, :inventory, :enabled
+  validates_presence_of :name, :description, :thumbnail, :price, :inventory
 
+  validates_inclusion_of :enabled, :in => [true, false]
   has_many :order_items, :dependent => :destroy
   has_many :orders, through: :order_items
   belongs_to :user

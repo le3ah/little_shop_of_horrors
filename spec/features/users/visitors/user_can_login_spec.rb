dc2dba7ad7fc9bc14fe401c1ef0d1ba8bb9ccc48
Tony Robertson <tonyr729@gmail.com>
2018-12-18 15:25:07 -0700

    Add login spec for merchant

--- spec/features/users/visitors/user_can_login_spec.rb
+++ spec/features/users/visitors/user_can_login_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 describe 'as a registered user' do
-  it "should login" do
-    user = User.create(name: "Anthony", password: "test", email: "Tony@gmail.com", address: "1609 Rose St. NW", city: "Cullman", state: "AL", zip: 35055)
-
+  it "should login users" do
+    user = create(:user)
+    
     visit root_path
     click_on "login"
 
@@ -21,13 +21,39 @@ describe 'as a registered user' do
     expect(page).to have_content("log out")
     expect(page).to have_content("Hooray!")
   end
-  it "should show error flash if incorrect login" do
-    user = User.create(name: "Anthony", password: "test", email: "Tony@gmail.com", address: "1609 Rose St. NW", city: "Cullman", state: "AL", zip: 35055)
+
+  it "should login merchants" do
+    user = create(:user, role: 1)
+    
     visit root_path
     click_on "login"
+
     expect(current_path).to eq(login_path)
+
+    fill_in "email", with: user.email
+    fill_in "password", with: user.password
+
+
+    click_on "submit"
+
+    expect(current_path).to eq(dashboard_path)
+
+    expect(page).to have_content("Welcome, merchant #{user.name}!")
+    expect(page).to have_content("log out")
+    
+  end
+
+  it "should show error flash if incorrect login" do
+    user = create(:user)
+
+    visit root_path
+    click_on "login"
+
+    expect(current_path).to eq(login_path)
+
     fill_in "email", with: user.email
     click_on "submit"
+
     expect(page).to_not have_content("Welcome, #{user.name}")
     expect(page).to have_content("Registered User Login")
     expect(page).to have_content("Oh no!")

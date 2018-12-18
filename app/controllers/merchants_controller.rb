5c7c5a1d615a82ed537425f2d296e2d7bea20090
Tony Robertson <tonyr729@gmail.com>
2018-12-18 15:25:17 -0700

    Add login implementation for merchant

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,8 @@
 class MerchantsController < ApplicationController
   def index
+  end
 
+  def show
+    @merchant = User.find(session[:user_id])
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Hooray! Welcome! 🎍"
-      redirect_to profile_path
+      redirect_to profile_path if user.role == "default"
+      redirect_to dashboard_path if user.role == "merchant"
     else
       flash.keep[:error] = "Oh no! Something went wrong. 🤯 🥀"
       render :new

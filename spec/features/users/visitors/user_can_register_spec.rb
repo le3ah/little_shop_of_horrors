fab095d04b22bf997acda323a06188c1207969e7
Leah Miller <lkmiller.87@gmail.com>
2018-12-17 18:39:58 -0700

    Adds test for error email flash

--- spec/features/users/visitors/user_can_register_spec.rb
+++ spec/features/users/visitors/user_can_register_spec.rb
@@ -39,5 +39,32 @@ describe 'as a visitor' do
     click_on 'Create User'
     expect(page).to have_content("User Registration Form")
   end
+  it "cannot repeat email addresses" do
+    user_1 = User.create(name: "Anthony", password: "test", email: "Tony@gmail.com", address: "1609 Rose St. NW", city: "Cullman", state: "AL", zip: 35055)
 
+    name = "Tony II"
+    email = "Tony@gmail.com"
+    address = "429 County Rd. 420"
+    city = "Cullman"
+    state = "AL"
+    zip = 35057
+
+    visit root_path
+
+    click_on 'Sign Up'
+    expect(current_path).to eq('/register')
+
+    fill_in :user_name, with: name
+    fill_in :user_email, with: email
+    fill_in :user_password, with: "test"
+    fill_in :user_confirm_password, with: "test"
+    fill_in :user_address, with: address
+    fill_in :user_city, with: city
+    fill_in :user_state, with: state
+    fill_in :user_zip, with: zip
+
+    click_on 'Create User'
+    expect(page).to have_content("User Registration Form")
+    expect(page).to have_content("Cannot repeat email address!")
+  end
 end

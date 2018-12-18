dedddcb015f8edefd44d6bd1afd5e3dd479dcd23
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-18 15:11:17 -0700

    adds checking for inclusion of enabled to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe User, type: :model do
         it { should validate_presence_of(:email) }
         it { should validate_presence_of(:password) }
         it { should validate_presence_of(:role) }
-        it { should validate_presence_of(:enabled) }
         it { should validate_presence_of(:address) }
         it { should validate_presence_of(:city) }
         it { should validate_presence_of(:zip) }
         it { should validate_presence_of(:state) }
+        it { should validate_inclusion_of(:enabled).in_array([true, false])}
     end
 
     describe 'should require uniqueness of' do

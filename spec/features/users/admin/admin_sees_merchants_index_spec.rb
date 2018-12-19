b7c5e7cf77622a529f3bd27dfd8e8d7d38b6e590
Nico <nicovigil1@gmail.com>
2018-12-18 17:07:55 -0700

    adds feature test for user role as admin as well as merchant view render

--- spec/features/users/admin/admin_sees_merchants_index_spec.rb
+++ spec/features/users/admin/admin_sees_merchants_index_spec.rb
@@ -16,7 +16,15 @@ RSpec.describe 'as an Admin' do
         end 
         
         it "can view the merchants show page and see the merchant's dashboard" do
-            
+            admin = User.new(name:"sweet christmas", email: 'test@test.com', password:"hownow", role: 2, 
+                address: "123 road street", city: "denver", zip:12345, state: "co")
+            merchant = create(:user, role: 1)
+
+            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+            visit admin_merchants_path
+            click_on()
+
         end 
         
     end

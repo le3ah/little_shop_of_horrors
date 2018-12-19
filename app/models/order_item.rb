0bbfa203f4af49441ad4f0c5d9b556d35f206079
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-19 15:31:04 -0700

    moves avg_fulfill_t to order_item instead of User

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,7 +1,15 @@
 class OrderItem < ApplicationRecord
   validates_presence_of :quantity, :price
   validates_inclusion_of :fulfilled, in: [true, false]
-  
+
   belongs_to :order
   belongs_to :item
+
+  def self.avg_fulfillment_time(item)
+    joins(items: :order_items)
+      .where("order_items.fulfilled = true")
+      .where(role: 1)
+      .group(item.id)
+      .select("avg(order_items.created_at - order_items.updated_at) as avg_f_time")
+  end
 end

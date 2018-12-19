904b11d6cdcbd2da0dd4082e940ddf1280db88b2
Nico <nicovigil1@gmail.com>
2018-12-19 16:33:09 -0700

    adds test for user navigation

--- spec/features/users/default/user_can_see_user_nav_links_spec.rb
+++ spec/features/users/default/user_can_see_user_nav_links_spec.rb
@@ -8,20 +8,54 @@ RSpec.describe "As a user" do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
         visit merchants_path 
+        
         within(".nav") do
             expect(page).to have_content("Profile")
             expect(page).to have_content("Orders")
-            expect(page).to have_content("Logout")
+            expect(page).to have_content("logout")
         end
 
         click_on('Profile')
         expect(current_path).to eq(profile_path)
         
         click_on('Orders')
-        expect(current_path).to eq(orders_path) 
-        
-        click_on('Logout')
-        expect(current_path).to eq(logout_path)
+        expect(current_path).to eq(profile_orders_path) 
+
+        click_on('logout')
+        expect(current_path).to eq(root_path)
     end
+
+    it "can not see login or sign up" do 
+        user = create(:user, name: "oh holy night", role:0)
+        
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit merchants_path
+
+        expect(page).to_not have_content("login")  
+        expect(page).to_not have_content("Sign Up")  
+    end 
+
+    it 'can see "sup, #{users name}"' do 
+        user = create(:user, name: "oh holy night", role:0)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        visit merchants_path
+
+        expect(page).to have_content("Logged in as #{user.name}")
+    end 
+
+    it "sad path - you can't see shit" do
+        
+        visit merchants_path 
+        
+        within(".nav") do
+            expect(page).to_not have_content("Profile")
+            expect(page).to_not have_content("Orders")
+            expect(page).to_not have_content("logout")
+        end
+
+    end
+
+
   end
 end

6eb8731e90db2ef4c9384da5074104604509c441
Nico <nicovigil1@gmail.com>
2018-12-19 14:25:39 -0700

    moves test for bad credential login by users

--- spec/features/users/visitors/user_can_login_spec.rb
+++ spec/features/users/visitors/user_can_login_spec.rb
@@ -64,20 +64,4 @@ describe 'as a registered user' do
     expect(page).to have_content("Hooray!")
     expect(page).to have_content("logout")
   end
-
-  it "should show error flash if incorrect login" do
-    user = create(:user)
-
-    visit root_path
-    click_on "login"
-
-    expect(current_path).to eq(login_path)
-
-    fill_in "email", with: user.email
-    click_on "submit"
-
-    expect(page).to_not have_content("Welcome, #{user.name}")
-    expect(page).to have_content("Registered User Login")
-    expect(page).to have_content("Oh no!")
-  end
 end

cad846dd14192f8f0af36e28f1405ac4647ff66c
Tony Robertson <tonyr729@gmail.com>
2018-12-18 18:53:19 -0700

    Add spec for user show page

---
+++ spec/features/users/default/user_sees_profile_page_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'as a registered user' do
+  it 'display profile page if I am logged in' do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit profile_path
+
+    expect(current_path).to eq profile_path
+    expect(page).to have_content("Welcome, #{user.name}!")
+    expect(page).to have_content("Email: #{user.email}")
+    expect(page).to have_content("Address: #{user.address}")
+    expect(page).to have_content("City: #{user.city}")
+    expect(page).to have_content("State: #{user.state}")
+    expect(page).to have_content("Zip: #{user.zip}")
+    expect(page).to have_link('Edit Profile', href: user_path(user))
+  end
+end

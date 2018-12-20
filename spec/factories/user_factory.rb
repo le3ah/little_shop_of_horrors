dd030e2f06372f259577d6cc4912fd143017297b
Leah Miller <lkmiller.87@gmail.com>
2018-12-20 15:25:03 -0700

    Adds FactoryBotDefine info for user and item

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -8,4 +8,10 @@ FactoryBot.define do
     sequence(:state) { |n| "State #{n}" }
     sequence(:zip) { |n| rand(10000..99999) }
   end
+
+  factory :inactive_user, parent: :user do
+    sequence(:name) { |n| "Inactive User #{n}"}
+    sequence(:email) { |n| "inactive_user_#{n}@gmail.com"}
+    active { false }
+  end 
 end

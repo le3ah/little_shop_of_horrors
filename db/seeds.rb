044bb3774659970fe44e9eea89bcac97914be2a2
Leah Miller <lkmiller.87@gmail.com>
2018-12-20 15:25:23 -0700

    Adds initial seed data for users and items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot_rails'
+
+include FactoryBot::Syntax::Methods
+
+OrderItem.destroy_all
+Order.destroy_all
+Item.destroy_all
+User.destroy_all
+
+admin = create(:user, role: 2)
+user = create(:user)
+merchant_1 = create(:user, role: 1)
+merchant_2 = create(:user, role: 1)
+merchant_3 = create(:user, role: 1)
+merchant_4 = create(:user, role: 1)
+
+item_1 = create(:item, user: merchant_1)

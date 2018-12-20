c32aaa38fd98b486cedcd44e252b32b3f473cfdb
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-20 13:02:45 -0700

    adds seed data and requiring factory bot

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "factory_bot_rails"
+
+OrderItem.destroy_all
+Item.destroy_all
+Order.destroy_all
+User.destroy_all
+
+include FactoryBot::Syntax::Methods
+
+user_1 = create(:user, city: 'San Diego', state: 'CA')
+user_2 = create(:user, city: 'San Diego', state: 'CA')
+user_3 = create(:user, city: 'Denver', state: 'CO')
+user_4 = create(:user, city: 'Denver', state: 'CO')
+user_9 = create(:user, city: 'Denver', state: 'FL')
+user_5 = create(:user, city: 'Miami', state: 'FL')
+user_6 = create(:user, city: 'Miami', state: 'FL')
+user_7 = create(:user, city: 'Oakland', state: 'CA')
+user_8 = create(:user, city: 'New York', state: 'NY')
+
+Order.create(status: "complete", user_id: user_1.id)
+Order.create(status: "complete", user_id: user_2.id)
+Order.create(status: "complete", user_id: user_3.id)
+Order.create(status: "complete", user_id: user_4.id)
+Order.create(status: "complete", user_id: user_5.id)
+Order.create(status: "complete", user_id: user_6.id)
+Order.create(status: "complete", user_id: user_7.id)
+Order.create(status: "complete", user_id: user_8.id)
+Order.create(status: "complete", user_id: user_9.id)

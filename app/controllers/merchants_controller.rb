52258b515674c167605d06c2a948d5129d2c5fd1
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-19 18:00:56 -0700

    adds proper top merchants to merchants#index

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,7 +1,7 @@
 class MerchantsController < ApplicationController
   def index
     @merchants = User.merchants
-    @top_sellers = User.merchants_by_revenue
+    @top_sellers = User.merchants_by_revenue(:top, 3)
     @fast_merchants = User.merchants_by_fullfillment_time(:top, 3)
     @slow_merchants = User.merchants_by_fullfillment_time(:bottom, 3)
   end

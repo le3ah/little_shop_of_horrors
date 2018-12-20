a78120672c524ca015fcd20e36ff6bc457d003c3
Tony Robertson <tonyr729@gmail.com>
2018-12-20 15:35:55 -0700

    Rename application controller method to create_cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
   protect_from_forgery with: :exception
 
   helper_method :current_user
-  before_action :cart
+  before_action :create_cart
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -11,7 +11,7 @@
     current_user && current_user.admin?
   end
 
-  def cart
+  def create_cart
     @cart ||= Cart.new(session[:cart])
   end
 end

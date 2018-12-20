359cb96f71090cc087bff21ae11bace878fa1729
Leah Miller <lkmiller.87@gmail.com>
2018-12-20 15:54:08 -0700

    Adds update to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,25 @@ merchant_3 = create(:user, role: 1)
 merchant_4 = create(:user, role: 1)
 
 item_1 = create(:item, user: merchant_1)
+item_2 = create(:item, user: merchant_2)
+item_3 = create(:item, user: merchant_3)
+item_4 = create(:item, user: merchant_4)
+create_list(:item, 10, user: merchant_1)
+
+order = create(:completed_order, user: user)
+create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_3, price: 3, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_4, price: 4, quantity: 1)
+
+order = create(:order, user: user)
+create(:order_item, order: order, item: item_1, price: 1, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1)
+
+order = create(:cancelled_order, user: user)
+create(:order_item, order: order, item: item_2, price: 2, quantity: 1)
+create(:order_item, order: order, item: item_3, price: 3, quantity: 1)
+
+order = create(:completed_order, user: user)
+create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1)

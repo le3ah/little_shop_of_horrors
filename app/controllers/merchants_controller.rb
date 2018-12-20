edd36c3a0b6021e20fbe64f07aec6cd2145fadaf
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-20 15:57:42 -0700

    adds passing top cities and states to merchants index

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -4,6 +4,8 @@ class MerchantsController < ApplicationController
     @top_sellers = User.merchants_by_revenue(:top, 3)
     @fast_merchants = User.merchants_by_fullfillment_time(:top, 3)
     @slow_merchants = User.merchants_by_fullfillment_time(:bottom, 3)
+    @top_states = User.top_states(3)
+    @top_cities = User.top_cities(3)
   end
 
   def show

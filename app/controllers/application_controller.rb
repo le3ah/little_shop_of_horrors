20bbd741062d960aaf432437476f78926fe11649
Nico <nicovigil1@gmail.com>
2018-12-20 15:26:29 -0700

    adds method for determining who can see the profile, order, and logged in status in the nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
  class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :power_users
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -14,4 +14,12 @@
   def cart
     @cart ||= Cart.new(session[:cart])
   end
+
+  def power_users
+    alpha_and_omega = (current_user && current_user.role == "default") || 
+    (current_user && current_user.role == 'admin' )  ||
+    (current_user && current_user.role == 'merchant')
+    alpha_and_omega
+  end 
+
 end

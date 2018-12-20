da35727657c1cffb317c38d5f58e20a54aea6106
Tony Robertson <tonyr729@gmail.com>
2018-12-20 15:32:27 -0700

    Add before_action method to  to give accesses to cart in nav view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
   protect_from_forgery with: :exception
 
   helper_method :current_user
-
+  before_action :cart
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end

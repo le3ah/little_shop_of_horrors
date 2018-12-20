cabc05c51b597a26eb8de3f951363147aba6453d
Tony Robertson <tonyr729@gmail.com>
2018-12-20 15:31:33 -0700

    Defind flash message in CartItem controller create method

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -7,6 +7,7 @@ class CartItemsController < ApplicationController
     item = Item.find(params[:item_id])   
     cart.add_item(item)
     session[:cart] = cart.data 
+    flash[:success] = "Item has been added to your cart"
     redirect_to items_path
   end
 end

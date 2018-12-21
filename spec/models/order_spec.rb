e7158f5a523f61a45ff43d8dd33df52abb3418f4
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-20 17:11:24 -0700

    adds test for .top_by_quantity class method to Orders

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -8,4 +8,85 @@ describe Order, type: :model do
   describe 'Relationships' do
     it { should belong_to(:user) }
   end
+
+  describe 'Class Methods' do
+    it '.top_by_quantity' do
+      u = create(:user)
+      m = create(:user, role: 1)
+
+      o_1 = Order.create(status: "complete", user_id: u.id)
+      o_2 = Order.create(status: "complete", user_id: u.id)
+      o_3 = Order.create(status: "complete", user_id: u.id)
+      o_4 = Order.create(status: "complete", user_id: u.id)
+
+      i = create(:item, user_id: m.id)
+      i_2 = create(:item, user_id: m.id)
+
+      OrderItem.create(
+        quantity: 1,
+        price: i.price,
+        fulfilled: true,
+        order_id: o_1.id,
+        item_id: i.id
+      )
+      OrderItem.create(
+        quantity: 1,
+        price: i_1.price,
+        fulfilled: true,
+        order_id: o_1.id,
+        item_id: i_2.id
+      )
+
+      OrderItem.create(
+        quantity: 2,
+        price: i.price,
+        fulfilled: true,
+        order_id: o_2.id,
+        item_id: i.id
+      )
+      OrderItem.create(
+        quantity: 2,
+        price: i_1.price,
+        fulfilled: true,
+        order_id: o_2.id,
+        item_id: i_2.id
+      )
+
+      OrderItem.create(
+        quantity: 3,
+        price: i.price,
+        fulfilled: true,
+        order_id: o_3.id,
+        item_id: i.id
+      )
+      OrderItem.create(
+        quantity: 3,
+        price: i_1.price,
+        fulfilled: true,
+        order_id: o_3.id,
+        item_id: i_2.id
+      )
+
+      OrderItem.create(
+        quantity: 4,
+        price: i.price,
+        fulfilled: true,
+        order_id: o_4.id,
+        item_id: i.id
+      )
+      OrderItem.create(
+        quantity: 4,
+        price: i_1.price,
+        fulfilled: true,
+        order_id: o_4.id,
+        item_id: i_2.id
+      )
+
+      top_orders = Order.top_by_quantity(3)
+
+      expect(top_orders[0].quantity).to eq(8)
+      expect(top_orders[1].quantity).to eq(6)
+      expect(top_orders[2].quantity).to eq(4)
+    end
+  end
 end

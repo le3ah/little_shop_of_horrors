a1e42a879a8711f90db755481da68f5c9f0009a7
Leah Miller <lkmiller.87@gmail.com>
2018-12-20 18:19:02 -0700

    Adds passing path test for login redirect

--- spec/features/users/default/user_is_redirected_if_logged_in_spec.rb
+++ spec/features/users/default/user_is_redirected_if_logged_in_spec.rb
@@ -1,1 +1,36 @@
+require 'rails_helper'
 
+describe  'as a registered user' do
+  context 'logged in as a default user' do
+    it "should be redirected to profile page" do
+      user_1 = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+      visit login_path
+
+      expect(current_path).to eq(profile_path)
+      # expect(page).to have_content("You are already logged in.")
+    end
+  end
+  context 'logged in as a merchant user' do
+    it "should be redirected to merchant dashboard page" do
+      merchant_1 = create(:user, role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_1)
+
+      visit login_path
+
+      expect(current_path).to eq(dashboard_path)
+      # expect(page).to have_content("You are already logged in.")
+    end
+  end
+  context 'logged in as an admin user' do
+    it "should be redirected to home page" do
+      admin_1 = create(:user, role: 2)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_1)
+
+      visit login_path
+
+      expect(current_path).to eq(root_path)
+      # expect(page).to have_content("You are already logged in.")
+    end
+  end
+end

003927ed62eb4c0d696fd182461c4b7d71f29ffa
Leah Miller <lkmiller.87@gmail.com>
2018-12-20 18:49:38 -0700

    Adds comment for test whose order may need to change

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -120,15 +120,15 @@ RSpec.describe User, type: :model do
         expect(m.enabled).to eq(true)
       end
 
-      it 'default class method finds all of the default users' do 
+      it 'default class method finds all of the default users' do
         user1 = create(:user)
         user2 = create(:user)
         merchant = create(:user, role: 1)
 
         expected = User.default
-
+        # FIX ME
         expect(expected).to eq([user1, user2])
-      end 
+      end
 
     end
   end

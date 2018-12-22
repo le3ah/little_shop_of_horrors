0386e2755785b21a4e27b47cf1ec2761908363de
Leah Miller <lkmiller.87@gmail.com>
2018-12-22 14:01:27 -0700

    Adds passing tests for viewing order item descriptions on profil show page Adds passing tests for viewing order item descriptions on profile show page

--- spec/features/orders/user_sees_order_show_page_spec.rb
+++ spec/features/orders/user_sees_order_show_page_spec.rb
@@ -5,21 +5,28 @@ describe  'Order Show Page' do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     m_1 = create(:user, role: 1)
-    o_1 = create(:completed_order, user_id: user.id)
+    o_1 = create(:order, user_id: user.id)
     item_2 = create(:item, price: 2, user_id: m_1.id)
-    order_item = create(:fulfilled_order_item, order: o_1, item: item_2, price: 2, quantity: 2, created_at: 7.days.ago, updated_at: 2.days.ago)
+    item_3 = create(:item, price: 4, user_id: m_1.id)
+    create(:fulfilled_order_item, order: o_1, item: item_2, price: 2, quantity: 2, created_at: 7.days.ago, updated_at: 2.days.ago)
+    create(:order_item, order: o_1, item: item_3, price: 4, quantity: 2, created_at: 7.days.ago, updated_at: 2.days.ago)
 
     visit profile_path
 
     click_link "View Order"
 
     expect(current_path).to eq("/profile/orders/#{o_1.id}")
-
-    o_1.order_items.each do |orderitem|
-      expect(page).to have_content("Order ID: #{orderitem.id}")
-      expect(page).to have_content("Order Created At: #{orderitem.created_at}")
-      expect(page).to have_content("Order Updated At: #{orderitem.updated_at}")
+save_and_open_page
+    o_1.order_items.each do |order_item|
+      expect(page).to have_content("Order ID: #{order_item.id}")
+      expect(page).to have_content("Order Created At: #{order_item.created_at}")
+      expect(page).to have_content("Order Updated At: #{order_item.updated_at}")
       expect(page).to have_content("Order Item Status: #{order_item.fulfilled? == true ? "fulfilled" : "not fulfilled"}")
+
+      expect(page).to have_content("Item Name: #{order_item.item.name}")
+      expect(page).to have_content("Item Description: #{order_item.item.description}")
+      expect(page).to have_content("Item Image: #{order_item.item.thumbnail}")
+      expect(page).to have_content("Item Quantity: #{order_item.quantity}")
     end
     expect(page).to have_content("Order Status: #{o_1.status}")
   end

88fe4a82b833d10ee7ada4cb4bbbdcf6aabec44e
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-22 13:47:54 -0700

    adds testing for edit page to have exisiting user content

--- spec/features/users/default/user_can_edit_profile_spec.rb
+++ spec/features/users/default/user_can_edit_profile_spec.rb
@@ -1,15 +1,28 @@
 require "rails_helper"
 
 describe "User Edit Profile" do
+  before :each do
+    @user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+
   it 'should link from profile page' do
-    user = create(:user)
-
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
     visit profile_path
-    
+
     click_link "Edit Profile"
 
-    expect(current_path).to eq(profile_edit)
+    expect(current_path).to eq(profile_edit_path)
+  end
+
+  it 'should show form with all user information' do
+    visit profile_edit_path
+
+    expect(find_field(:user_name).value).to eq @user.name
+    expect(find_field(:user_email).value).to eq @user.email
+    expect(find_field(:user_address).value).to eq @user.address
+    expect(find_field(:user_city).value).to eq @user.city
+    expect(find_field(:user_state).value).to eq @user.state
+    expect(find_field(:user_zip).value.to_i).to eq @user.zip
   end
 end

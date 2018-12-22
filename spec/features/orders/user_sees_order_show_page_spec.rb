1b6d8e16748b37260b63788032e57356d010825b
Leah Miller <lkmiller.87@gmail.com>
2018-12-22 15:13:49 -0700

    Adds test for it should not have certain content on order show page

--- spec/features/orders/user_sees_order_show_page_spec.rb
+++ spec/features/orders/user_sees_order_show_page_spec.rb
@@ -6,6 +6,7 @@ describe  'Order Show Page' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     m_1 = create(:user, role: 1)
     o_1 = create(:order, user_id: user.id)
+    item_1 = create(:item, price: 1, user_id: m_1.id)
     item_2 = create(:item, price: 2, user_id: m_1.id)
     item_3 = create(:item, price: 4, user_id: m_1.id)
     create(:fulfilled_order_item, order: o_1, item: item_2, price: 2, quantity: 2, created_at: 7.days.ago, updated_at: 2.days.ago)
@@ -29,9 +30,13 @@ describe  'Order Show Page' do
       expect(page).to have_content("Item Quantity: #{order_item.quantity}")
       expect(page).to have_content("Item Price: $#{order_item.price}")
       expect(page).to have_content("Item Subtotal: $#{order_item.subtotal}")
+
+
     end
     expect(page).to have_content("Total Item Quantity: #{o_1.quantity_of_order}")
     expect(page).to have_content("Order Grand Total: $#{o_1.grand_total}")
     expect(page).to have_content("Order Status: #{o_1.status}")
+
+    expect(page).to_not have_content("Item Name: #{item_1.name}")
   end
 end

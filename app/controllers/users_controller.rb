c7f53d060158518036f9fb85b549587a56e148d8
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-22 15:18:54 -0700

    adds edit and update user methods, must use .password to update that

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,21 @@ class UsersController < ApplicationController
     @user = current_user || User.find(session[:user_id])
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    params[:user].each do |attribute, value|
+      next if value.blank? || @user[attribute] == value
+      @user[attribute] = value unless attribute =~ /password/
+      @user.password = value if attribute =~ /password/
+    end
+    @user.save
+    redirect_to profile_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :password, :email, :address, :city, :state, :zip)

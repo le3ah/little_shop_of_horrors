388d736977707b6677487685bdb71fead70dec44
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-22 15:18:01 -0700

    adds testing user can update all fields (including password)

--- spec/features/users/default/user_can_edit_profile_spec.rb
+++ spec/features/users/default/user_can_edit_profile_spec.rb
@@ -15,7 +15,7 @@ describe "User Edit Profile" do
     expect(current_path).to eq(profile_edit_path)
   end
 
-  it 'should show form with all user information' do
+  it 'should show form with all user information except password' do
     visit profile_edit_path
 
     expect(find_field(:user_name).value).to eq @user.name
@@ -24,5 +24,68 @@ describe "User Edit Profile" do
     expect(find_field(:user_city).value).to eq @user.city
     expect(find_field(:user_state).value).to eq @user.state
     expect(find_field(:user_zip).value.to_i).to eq @user.zip
+
+    expect(find_field(:user_password).value).to be_nil
+  end
+
+  it 'should be able to update some user information' do
+    visit profile_edit_path
+
+    fill_in :user_name, with: "John"
+    fill_in :user_email, with: "john@gmail.com"
+
+    click_on "Update User"
+
+    expect(current_path).to eq(profile_path)
+    expect(page).to have_content("Welcome, John!")
+    expect(page).to have_content("Email: john@gmail.com")
+    expect(@user.name).to eq("John")
+    expect(@user.email).to eq("john@gmail.com")
+
+    expect(page).to have_content(@user.address)
+    expect(page).to have_content(@user.city)
+    expect(page).to have_content(@user.state)
+    expect(page).to have_content(@user.zip)
+    expect(page).to have_content("You successfully edited your profile!")
+  end
+
+  it 'should be able to update all user information' do
+    visit profile_edit_path
+
+    name = "Tony II"
+    email = "Tony@gmail.com"
+    address = "429 County Rd. 420"
+    city = "Cullman"
+    state = "AL"
+    zip = 35057
+
+    fill_in :user_name, with: name
+    fill_in :user_email, with: email
+    fill_in :user_password, with: "test"
+    fill_in :user_confirm_password, with: "test"
+    fill_in :user_address, with: address
+    fill_in :user_city, with: city
+    fill_in :user_state, with: state
+    fill_in :user_zip, with: zip
+
+    click_on "Update User"
+
+    expect(current_path).to eq(profile_path)
+    expect(page).to have_content("Welcome, #{name}!")
+    expect(page).to have_content("Email: #{email}")
+    expect(page).to have_content("Address: #{address}")
+    expect(page).to have_content("City: #{city}")
+    expect(page).to have_content("State: #{state}")
+    expect(page).to have_content("Zip: #{zip}")
+
+    expect(@user.name).to eq(name)
+    expect(@user.email).to eq(email)
+    expect(@user.password).to eq("test")
+    expect(@user.address).to eq(address)
+    expect(@user.city).to eq(city)
+    expect(@user.state).to eq(state)
+    expect(@user.zip).to eq(zip)
+
+    expect(page).to have_content("You successfully edited your profile!")
   end
 end

76bf73f384ee625ed68d9efd3c11016c30340b42
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-23 13:51:41 -0800

    adds timestamps to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20181218001734) do
   create_table "orders", force: :cascade do |t|
     t.string "status"
     t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 

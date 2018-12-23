51eebc67737418d397265cd3c0c6fe7adfd394fb
Leah Miller <lkmiller.87@gmail.com>
2018-12-22 18:11:52 -0700

    Adds factory bot admin creation

--- spec/features/users/admin/admin_can_see_specific_nav_links_spec.rb
+++ spec/features/users/admin/admin_can_see_specific_nav_links_spec.rb
@@ -1,52 +1,52 @@
-describe 'admin can see specific links in the nav bar' do 
+describe 'admin can see specific links in the nav bar' do
     it "can see specific admin links in the nav" do
         user = create(:user, name: "oh holy night", role:2)
-        
+
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-        visit merchants_path 
+        visit merchants_path
 
         within(".nav") do
             expect(page).to have_content("Orders")
             expect(page).to have_content("Logout")
-            expect(page).to have_content("Users") 
+            expect(page).to have_content("Users")
             expect(page).to_not have_content("Shopping Cart")
         end
-        
+
         click_on('Orders')
-        expect(current_path).to eq(profile_orders_path) 
+        expect(current_path).to eq(profile_orders_path)
 
         click_on('Logout')
         expect(current_path).to eq(root_path)
 
         click_on('Users')
-        expect(current_path).to eq(admin_users_path) 
+        expect(current_path).to eq(admin_users_path)
     end
 
-    it "can not see login or sign up" do 
+    it "can not see login or sign up" do
         admin = create(:user, name: "oh holy night", role:2)
-        
+
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
         visit admin_merchants_path
 
-        expect(page).to_not have_content("login")  
-        expect(page).to_not have_content("Sign Up")  
-    end 
+        expect(page).to_not have_content("login")
+        expect(page).to_not have_content("Sign Up")
+    end
 
-    it 'can see "sup, #{users name}"' do 
+    it 'can see "sup, #{users name}"' do
         admin = create(:user, name: "oh holy night", role:2)
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-        
+
         visit admin_merchants_path
 
         expect(page).to have_content("Logged in as #{admin.name}")
-    end 
+    end
 
     it "sad path - visiot cannot see admin merchants path" do
-        
-        visit admin_merchants_path 
-        
+
+        visit admin_merchants_path
+
         within(".nav") do
             expect(page).to_not have_content("Profile")
             expect(page).to_not have_content("Orders")
@@ -64,13 +64,8 @@ describe 'admin can see specific links in the nav bar' do
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-        visit admin_merchants_path 
+        visit admin_merchants_path
 
         expect(page).to have_content("The page you were looking for doesn't exist.")
-
     end
-
-
-
-
 end

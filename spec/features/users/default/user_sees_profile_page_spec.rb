a6151e75f0f5343c7b993990ff3455e769c6f145
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-23 12:57:51 -0800

    adds testing for linking to order show pages and order info on user profile

--- spec/features/users/default/user_sees_profile_page_spec.rb
+++ spec/features/users/default/user_sees_profile_page_spec.rb
@@ -1,20 +1,75 @@
 require 'rails_helper'
 
-describe 'as a registered user' do
-  it 'displays profile page if I am logged in' do
-    user = create(:user)
+describe 'Default User Profile Page' do
+  before :each do
+    @u = create(:user)
+    @m = create(:user, role: 1)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    @o_1 = create(:order, user: @u)
+    @o_2 = create(:completed_order, user: @u)
+    @o_3 = create(:cancelled_order, user: @u)
+
+    @i_1 = create(:item, enabled: true, user: @m)
+    @i_2 = create(:item, enabled: true, user: @m)
+    @i_3 = create(:item, enabled: true, user: @m)
+
+    create(:order_item, order: @o_1, item: @i_1)
+    create(:order_item, order: @o_2, item: @i_2)
+    create(:order_item, order: @o_3, item: @i_3)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(u)
 
     visit profile_path
+  end
 
+  it 'displays user info on profile page' do
     expect(current_path).to eq profile_path
-    expect(page).to have_content("Welcome, #{user.name}!")
-    expect(page).to have_content("Email: #{user.email}")
-    expect(page).to have_content("Address: #{user.address}")
-    expect(page).to have_content("City: #{user.city}")
-    expect(page).to have_content("State: #{user.state}")
-    expect(page).to have_content("Zip: #{user.zip}")
+    expect(page).to have_content("Welcome, #{@u.name}!")
+    expect(page).to have_content("Email: #{@u.email}")
+    expect(page).to have_content("Address: #{@u.address}")
+    expect(page).to have_content("City: #{@u.city}")
+    expect(page).to have_content("State: #{@u.state}")
+    expect(page).to have_content("Zip: #{@u.zip}")
     expect(page).to have_link('Edit Profile', href: profile_edit_path)
+  end
+
+  context 'when user has ordered items' do
+    it 'should link to order show page' do
+      click_link "View Order ##{@o_1.id}"
+      expect(current_path).to eq(profile_order_path(@o_1))
+
+      visit profile_path
+
+      click_link "View Order ##{@o_2.id}"
+      expect(current_path).to eq(profile_order_path(@o_2))
+
+      visit profile_path
+
+      click_link "View Order ##{@o_3.id}"
+      expect(current_path).to eq(profile_order_path(@o_3))
+    end
+
+    it 'should show order information' do
+      expect(page).to have_content("View Order ##{@o_1.id}")
+      expect(page).to have_content("Created at: #{@o_1.created_at}")
+      expect(page).to have_content("Updated at: #{@o_1.updated_at}")
+      expect(page).to have_content("#{@o_1.status}")
+      expect(page).to have_content("#{@o_1.quantity_of_order}")
+      expect(page).to have_content("#{@o_1.grand_total}")
+
+      expect(page).to have_content("View Order ##{@o_2.id}")
+      expect(page).to have_content("Created at: #{@o_2.created_at}")
+      expect(page).to have_content("Updated at: #{@o_2.updated_at}")
+      expect(page).to have_content("Status: #{@o_2.status}")
+      expect(page).to have_content("Item Count: #{@o_2.quantity_of_order}")
+      expect(page).to have_content("Grand Total: #{@o_2.grand_total}")
+
+      expect(page).to have_content("View Order ##{@o_2.id}")
+      expect(page).to have_content("Created at: #{@o_2.created_at}")
+      expect(page).to have_content("Updated at: #{@o_2.updated_at}")
+      expect(page).to have_content("Status: #{@o_2.status}")
+      expect(page).to have_content("Item Count: #{@o_2.quantity_of_order}")
+      expect(page).to have_content("Grand Total: #{@o_2.grand_total}")
+    end
   end
 end

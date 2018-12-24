85dc7b6cd38a646d249b272a2ba3d18fe54aade7
Leah Miller <lkmiller.87@gmail.com>
2018-12-24 10:25:53 -0700

    Adds passing navigation test for admin merchant disables merchant

---
+++ spec/features/users/admin/admin_merchant_can_disable_spec.rb
@@ -0,0 +1,18 @@
+require  'rails_helper'
+
+describe 'As an admin merchant' do
+  context 'when I visit the merchant index page' do
+    it "should disable an enabled merchant" do
+      @admin = create(:user, role: 2)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+      @merchant_1 = create(:user, role: 1)
+      @merchant_2 = create(:user, role: 1, enabled: false)
+
+      visit admin_merchants_path
+      click_button "disable"
+
+      expect(current_path).to eq(admin_merchants_path)
+    end
+  end
+end

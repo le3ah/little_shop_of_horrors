adb4381bef52ee08cfd29170e0329c0df58e0601
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-24 15:58:18 -0800

    fixes spacing and adds success message either way - enabled or disabled

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,20 +1,19 @@
 class Admin::MerchantsController < Admin::BaseController
-    def index
-        @merchants = User.where('role = 1')
-    end
+  def index
+      @merchants = User.where('role = 1')
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def toggle_status
-        user = User.find(params[:user_id])
+  def toggle_status
+    user = User.find(params[:user_id])
+    user.switch_enabled
+    
+    flash[:success] = user.enabled? ?
+      "#{user.name}'s account is now enabled." :
+      "#{user.name}'s account is now disabled."
 
-        user.switch_enabled
-
-        redirect_to admin_merchants_path
-        if !user.enabled?
-          flash[:success] = "#{user.name}'s account is now disabled."
-        end
-    end
-
+    redirect_to admin_merchants_path
+  end
 end

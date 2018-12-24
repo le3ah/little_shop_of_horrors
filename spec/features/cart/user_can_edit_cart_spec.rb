1222a8e012122968bd590b20b6afea4ecc370af1
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-24 12:50:25 -0800

    unskip tests, fixes typos

--- spec/features/cart/user_can_edit_cart_spec.rb
+++ spec/features/cart/user_can_edit_cart_spec.rb
@@ -5,8 +5,8 @@ describe "Editing Cart" do
     context 'when there are items in the cart' do
       before :each do
         user = create(:user, role: 1)
-        @item = create(:item, user_id: user.id, inventory: 2)
-        @item_2 = create(:item, user_id: user.id)
+        @item = create(:item, user_id: user.id)
+        @item_2 = create(:item, user_id: user.id, inventory: 2)
 
         visit item_path(@item)
         click_button "Add to Cart"
@@ -18,7 +18,7 @@ describe "Editing Cart" do
         visit cart_path
       end
 
-      xit 'has a button that removes an item' do
+      it 'has a button that removes an item' do
         expect(page).to have_selector(:link_or_button, "Remove", count: 2)
         click_button "rem-cart-item-0"
 
@@ -27,7 +27,7 @@ describe "Editing Cart" do
         expect(page).to have_content("Your cart has been updated")
       end
 
-      xit 'has a button that increments an item quantity' do
+      it 'has a button that increments an item quantity' do
         expect(page).to have_selector(:link_or_button, "+", count: 2)
         click_button "plus-cart-item-0"
 
@@ -36,7 +36,7 @@ describe "Editing Cart" do
         expect(page).to have_content("Your cart has been updated")
       end
 
-      xit 'has a button that decrements an item quantity' do
+      it 'has a button that decrements an item quantity' do
         expect(page).to have_selector(:link_or_button, "-", count: 2)
         click_button "minus-cart-item-1"
 
@@ -47,15 +47,15 @@ describe "Editing Cart" do
 
       it "can't increment item quantity past merchant's inventory" do
         expect(page).to have_content("#{@item_2.name} 2")
-        click_button "plus_cart-item-1"
+        click_button "plus-cart-item-1"
 
         expect(page).to have_content("#{@item_2.name} 2")
         expect(page).to have_content("Not enough item quantity to fulfill that amount")
       end
 
-      xit 'automatically removes item when quantity reaches 0' do
+      it 'automatically removes item when quantity reaches 0' do
         expect(page).to have_content("#{@item.name} 1")
-        click_button "minus_cart-item-1"
+        click_button "minus-cart-item-0"
 
         expect(page).to_not have_content("#{@item.name}")
         expect(page).to have_content("Your cart has been updated")

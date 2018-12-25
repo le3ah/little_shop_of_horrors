675c2cf7293f5a82b2ddbcdc3ca68d7003714850
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-25 09:06:50 -0800

    adds flash message and redirection to OrdersController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,7 @@ class OrdersController < ApplicationController
   def create
     order = Order.create(user_id: params[:user_id])
     OrderItem.checkout_cart(@cart, order.id)
+    flash[:success] = "Order was created!"
+    redirect_to profile_path
   end
 end

6af80ed3b98493a84ad22f5cad53911eaeecc2fa
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-25 09:06:09 -0800

    changed user name for tests

--- spec/features/cart/user_can_checkout_spec.rb
+++ spec/features/cart/user_can_checkout_spec.rb
@@ -4,8 +4,8 @@ describe "Cart Checkout" do
   context "when a registered user with items in cart" do
     before :each do
       m = create(:user, role: 1)
-      u = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(u)
+      @u = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@u)
       @i_1 = create(:item, user_id: m.id)
       @i_2 = create(:item, user_id: m.id)
 
@@ -24,7 +24,7 @@ describe "Cart Checkout" do
     it 'checking out creates a pending order' do
       click_button "Checkout"
 
-      order = user.orders.first
+      order = @u.orders.first
       expect(Order.count).to eq(1)
       expect(order.items.count).to eq(2)
       expect(order.status).to eq("pending")
@@ -37,7 +37,7 @@ describe "Cart Checkout" do
 
       expect(current_path).to eq(profile_path)
 
-      order = user.orders.first
+      order = @u.orders.first
 
       order.order_items.each do |order_item|
         expect(page).to have_content("Order Item ID: #{order_item.id}")

d8e0ce4b868c80724f02fe5ad650ef99a9e26043
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-25 13:48:27 -0800

    adds testing return_inventory for Item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -89,5 +89,33 @@ describe Item, type: :model do
       expect(Item.popular_items(:top, 5)).to eq([i_4, i_5, i_2, i_3, i_1])
       expect(Item.popular_items(:bottom, 5)).to eq([i_6, i_1, i_3, i_2, i_5])
     end
+
+    it '.return_inventory' do
+      u = create(:user)
+      m = create(:user, role: 1)
+
+      o = create(:order, user: u)
+      i_1 = create(:item, user: m)
+      i_2 = create(:item, user: m)
+      i_3 = create(:item, user: m)
+
+      o_i_1 = create(:fulfilled_order_item, order: o, item: i_1)
+      o_i_2 = create(:fulfilled_order_item, order: o, item: i_2)
+      o_i_3 = create(:order_item, order: o, item: i_3)
+
+      before_inventories = [i_1.inventory, i_2.inventory, i_3.inventory]
+      items = [i_1, i_2, i_3]
+
+      Item.return_inventory(
+        [
+          [o_i_1.item_id, o_i_1.quantity],
+          [o_i_2.item_id, o_i_2.quantity],
+          [o_i_3.item_id, o_i_3.quantity]
+      ])
+
+      expect(items[0].inventory).to eq(before_inventories[0] + o_i_1.quantity)
+      expect(items[1].inventory).to eq(before_inventories[1] + o_i_2.quantity)
+      expect(items[2].inventory).to eq(before_inventories[2] + o_i_3.quantity)
+    end
   end
 end

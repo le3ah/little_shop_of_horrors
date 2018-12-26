2b28997c657433fcb337fe2a52cd286ed48a06e9
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-26 08:02:45 -0800

    fixes spacing, adds user to show route

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,15 @@
 class Admin::UsersController < Admin::BaseController
-    def index
-        @default_users = User.default
-    end 
+  def index
+    @default_users = User.default
+  end
 
-    def show
+  def show
+    @user = User.find(params[:id])
+  end
 
-    end
-
-    def toggle_user
-        user = User.find(params[:user_id])
-        user.switch_enabled
-        redirect_to admin_users_path
-    end
-
-end 
+  def toggle_user
+    user = User.find(params[:user_id])
+    user.switch_enabled
+    redirect_to admin_users_path
+  end
+end

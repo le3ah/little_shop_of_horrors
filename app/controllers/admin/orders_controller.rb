212703b05835f929c54df136c61aa3832b8c971b
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-26 09:19:17 -0800

    adds redirect to admin_user show using user id from params

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,6 +3,6 @@ class Admin::OrdersController < Admin::BaseController
     Order.cancel(params[:id])
 
     flash[:success] = "Order has been cancelled."
-    redirect_to profile_path
+    redirect_to admin_user_path(params[:user_id])
   end
 end

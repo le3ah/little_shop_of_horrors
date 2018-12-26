74cdb3105172b1de98abcce7e971e2b9705af3ff
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-26 08:33:07 -0800

    adds destroy route to Admin::Orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,8 @@
 class Admin::OrdersController < Admin::BaseController
   def destroy
-    
+    Order.cancel(params[:id])
+
+    flash[:success] = "Order has been cancelled."
+    redirect_to profile_path
   end
 end

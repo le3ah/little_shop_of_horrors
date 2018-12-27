1d6eec91218682f2547bbe6ad67248953438aaa0
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-27 10:10:44 -0800

    adds logged in admin to test

--- spec/features/users/admin/admin_sees_redirect_for_users_spec.rb
+++ spec/features/users/admin/admin_sees_redirect_for_users_spec.rb
@@ -5,6 +5,7 @@ describe "Admin merchant/user show page redirect" do
     @u = create(:user)
     @m = create(:user, role: 1)
     @a = create(:user, role: 2)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@a)
   end
 
   it 'redirects to admin_user when visiting admin_merchant if id matches a default user' do

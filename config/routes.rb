778985f9718d547ce4282f2f3f2fc73dacf5dc03
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-27 15:30:31 -0800

    adds show route to admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :merchants, only: [:index, :show]
     resources :users, only: [:index, :show, :edit]
-    resources :orders, only: [:destroy]
+    resources :orders, only: [:destroy, :show]
     post '/toggle', to: "merchants#toggle_status"
     post '/toggle-user', to: "users#toggle_user"
     post '/toggle-role', to: "merchants#toggle_role"

c78e549babd70b99609dfbdce1d3a101d545a3e2
Nico <nicovigil1@gmail.com>
2018-12-27 12:26:31 -0600

    adds test for creating a new merchant;

--- spec/features/users/merchant/merchant_create_spec.rb
+++ spec/features/users/merchant/merchant_create_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 describe "As a Merchant" do
-  context "I can create" do
-    it "specific things" do
+  context "on dashboard/items i see" do
+    it "a link to create an item" do
+      visit dashboard_items_path
 
+      click_on 'add new item'
+
+      expect(current_path).to eq(dashboard_items_new_path) 
+      
+      fill_in "Name",	with: "Testing"
+      fill_in "Description",	with: "123" 
+      fill_in "thumbnail",	with: "FIXME: SHOULD BE IMAGE" 
+      fill_in "Price",	with: "11" 
+      fill_in "Inventory",	with: "123" 
+      click_on "Create Item"
+
+      expect(current_path).to eq(dashboard_items_path)
     end
   end
 end

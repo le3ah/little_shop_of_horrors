0923abfa59a93028af36eedde612667a552251f2
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-27 17:31:25 -0800

    adds toggle_enabled to Admin::BaseC to dry up code

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -6,4 +6,13 @@ class Admin::BaseController < ApplicationController
   def require_admin
     render_404 unless current_admin?
   end
+
+  def toggle_enabled(user_id)
+    user = User.find(user_id)
+    user.switch_enabled
+
+    flash[:success] = user.enabled? ?
+      "#{user.name}'s account is now enabled." :
+      "#{user.name}'s account is now disabled."
+  end
 end

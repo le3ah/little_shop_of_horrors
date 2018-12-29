01d709892efe099c0124d2cf2479ff1ebe56348a
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-29 12:23:54 -0800

    adds before_action merchant_or_admin?, CRUD methods to ItemsC

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :merchant_or_admin?
+  skip_before_action :merchant_or_admin, only: [:index, :show]
+
   def index
     @items = Item.all
   end
@@ -8,4 +11,59 @@ class ItemsController < ApplicationController
     @average_fulfillment_time = @item.average_fulfillment_time
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    render_404 unless current_merchant?
+
+    thumbnail = params[:item][:thumbnail]
+    File.open(Rails.root.join('app', 'assets', 'images', thumbnail.original_filename), 'wb') do |file|
+      file.write(thumbnail.read)
+    end
+
+    current_user.items.create(item_params)
+    redirect_to dashboard_items_path
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    update_item(@item, params[:item])
+    flash[:success] = "You successfully edited that item!"
+    redirect_to dashboard_items_path if current_merchant?
+    redirect_to items_path if current_admin?
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to dashboard_items_path if current_merchant?
+    redirect_to items_path if current_admin?
+  end
+
+  private
+
+  def item_params
+    thing = params[:item][:thumbnail].original_filename
+    params[:item][:thumbnail] = thing
+    params.require(:item).permit(:name, :description, :price, :thumbnail, :inventory)
+  end
+
+  def update_item(item, params)
+    params.each do |attribute, value|
+      next if value.blank? || item[attribute] == value
+      item[attribute] = value
+    end
+    item.save
+  end
+
+  def merchant_or_admin?
+    render_404 unless current_admin? || current_merchant?
+  end
 end

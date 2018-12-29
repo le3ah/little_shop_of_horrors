79f4929f4f35c68a5df5359fc2b678c0cc514db0
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-29 14:40:04 -0800

    adds order_items_for_merchant to Dashboard::OrderC#show

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -5,6 +5,7 @@ class Dashboard::OrdersController < Dashboard::BaseController
 
   def show
     @order = Order.find(params[:id])
+    @order_items = @order.order_items_for_merchant(params(:merchant_id))
     @customer = @order.user
   end
 end

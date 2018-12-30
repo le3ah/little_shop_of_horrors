32a9ea04caad89e1c086a2aaa485c25e96513fc9
Leah Miller <lkmiller.87@gmail.com>
2018-12-30 12:45:01 -0700

    Removes merchant stats test from reads feature file

--- spec/features/users/merchant/merchant_read_spec.rb
+++ spec/features/users/merchant/merchant_read_spec.rb
@@ -64,42 +64,11 @@ describe "As a Merchant" do
       end
     end
 
-    context "Statistics" do
-      it 'can display the top 5 items sold by quantity on show page' do
-        merchant = create(:user, role: 1)
-        item_1 = create(:item, user:merchant)
-        item_2 = create(:item, user:merchant)
-        item_3 = create(:item, user:merchant)
-        item_4 = create(:item, user:merchant)
-        item_5 = create(:item, user:merchant)
-        item_6 = create(:item, user:merchant)
-        order = create(:completed_order, user_id:merchant.id)
-        create(:fulfilled_order_item,  order:order, item: item_1, price: 1, quantity: 25)
-        create(:fulfilled_order_item,  order:order, item: item_2, price: 1, quantity: 25)
-        create(:fulfilled_order_item,  order:order, item: item_3, price: 1, quantity: 25)
-        create(:fulfilled_order_item,  order:order, item: item_4, price: 1, quantity: 25)
-        create(:fulfilled_order_item,  order:order, item: item_5, price: 1, quantity: 25)
-        create(:fulfilled_order_item,  order:order, item: item_6, price: 1, quantity: 1)
-
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
-
-        visit dashboard_path
-
-        within "#top-5-items" do
-          expect(page).to have_content(item_1.name)
-          expect(page).to have_content(item_2.name)
-          expect(page).to have_content(item_3.name)
-          expect(page).to have_content(item_4.name)
-          expect(page).to have_content(item_5.name)
-        end
-      end
-    end
-
     context "/dashboard/items" do
       it 'can see a toggled button for enable/disable next to each item' do
         merchant = create(:user, role: 1)
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
-        user = create(:user)
+
         item_1 = create(:item, user: merchant, thumbnail: 'oldguy.jpeg')
 
         visit dashboard_items_path

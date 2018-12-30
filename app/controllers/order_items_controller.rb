4bd1fc2605940d198f933a2d4b413d4b991f0c37
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-29 21:23:13 -0800

    adds updating order status and decreasing inventory for item

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,10 +1,14 @@
 class OrderItemsController < ApplicationController
   def fulfill_order_item
     merchant_or_admin?
+
     order_item = OrderItem.find(params[:order_item_id])
     order_item.fulfilled = true
     order_item.save
-    order_item.order.check_complete
+    order_item.order.update_status
+    flash[:success] = "Order Item has been fulfilled!"
+
+    order_item.item.decrease_inventory(order_item.quantity)
     redirect_to dashboard_order_path(order_item.order)
   end
 end

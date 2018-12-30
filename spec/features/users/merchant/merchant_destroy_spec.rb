e9d1e7c5e113421308efc2fd02bfcaca4972e2c1
Nico <nicovigil1@gmail.com>
2018-12-29 18:49:23 -0600

    adds test for deleting items on dashboard/items

--- spec/features/users/merchant/merchant_destroy_spec.rb
+++ spec/features/users/merchant/merchant_destroy_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 describe "As a merchant" do
-    context "I can destroy" do
-        it "different things" do
+    context "on /dashboard/items" do
+        it "can delete an item & see a flash message" do
+            merchant = create(:user, role: 1)
+            item = create(:item, user:merchant, thumbnail:"oldguy.jpeg")
+            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
+            visit dashboard_items_path
+            save_and_open_page
+            within "#item-#{item.id}" do 
+                expect(page).to have_button("delete")
+                click_button "delete"
+            end 
+            save_and_open_page
+            expect(current_path).to eq(dashboard_items_path)
+            expect(page).to have_content("item successfully deleted!") 
+
 
         end
     end

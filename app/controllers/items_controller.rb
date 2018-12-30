8a44d5ea72eaa67d1cbcf0f1a876c1d2727857e0
Nico <nicovigil1@gmail.com>
2018-12-29 20:47:47 -0600

    minor fixes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,16 +19,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def create
-    render_404 unless current_merchant?
-
+  def create    
     thumbnail = params[:item][:thumbnail]
-    File.open(Rails.root.join('app', 'assets', 'images', thumbnail.original_filename), 'wb') do |file|
-      file.write(thumbnail.read)
-    end
 
-    current_user.items.create(item_params)
-    redirect_to dashboard_items_path
+    if thumbnail
+      File.open(Rails.root.join('app', 'assets', 'images', thumbnail.original_filename), 'wb') do |file|
+        file.write(thumbnail.read)
+      end
+    end 
+    @merchant = current_user
+    @item = @merchant.items.new(item_params)
+    if @item.save
+      flash[:success] = "Item added!"
+      redirect_to dashboard_items_path
+    else
+      flash[:error] = "Something went wrong!"
+      render template: 'merchants/new'
+    end
   end
 
   def update
@@ -56,9 +63,13 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    thing = params[:item][:thumbnail].original_filename
-    params[:item][:thumbnail] = thing
-    params.require(:item).permit(:name, :description, :price, :thumbnail, :inventory)
+    if params[:item][:thumbnail]
+      thing = params[:item][:thumbnail].original_filename
+      params[:item][:thumbnail] = thing
+      params.require(:item).permit(:name, :description, :price, :thumbnail, :inventory)
+    else 
+      params.require(:item).permit(:name, :description, :thumbnail, :price, :inventory)
+    end
   end
 
   def update_item(item, params)

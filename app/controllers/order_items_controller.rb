da39e7609f2ffe2d11dfd6ae76630e94576ddeb4
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-29 19:58:45 -0800

    adds fulfill_order_item to OrderItemsC

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,3 +1,10 @@
 class OrderItemsController < ApplicationController
-
-end 
+  def fulfill_order_item
+    merchant_or_admin?
+    order_item = OrderItem.find(params[:order_item_id])
+    order_item.fulfilled = true
+    order_item.save
+    order_item.order.check_complete
+    redirect_to dashboard_order_path(item.order)
+  end
+end

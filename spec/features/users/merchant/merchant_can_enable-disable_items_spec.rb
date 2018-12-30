9a11ef06dcdc392179c145f0cb763554343ada4b
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-29 17:13:31 -0800

    adds testing for redirect, flashm, and updated item enabled - when clicking enable/disable

---
+++ spec/features/users/merchant/merchant_can_enable-disable_items_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+describe "Merchant Enable/Disable Item" do
+  context 'when merchant visits dashboard_items page' do
+    before :each do
+      @m = create(:user, role: 1)
+      @i_1 = create(:item, user: @m, enabled: true)
+      @i_2 = create(:item, user: @m)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m)
+      visit dashboard_items
+    end
+
+    it 'shows button to enable or disable and clicking redirects back to dashboard_items' do
+      expect(page).to have_button("Enable", count: 1)
+      expect(page).to have_button("Disable", count: 1)
+
+      click_button "Enable"
+      expect(current_path).to eq(dashboard_items)
+      expect(page).to have_button("Disable", count: 2)
+
+      first(:button, "Disable").click
+      expect(current_path).to eq(dashboard_items)
+    end
+
+    it 'clicking button to enable or disable updates item and show appropriate flash m' do
+      click_button "Enable"
+      expect(page).to have_content("Item is available for sale!")
+
+      @i_2.reload
+      expect(@i_2.enabled).to be_truthy
+
+      first(:button, "Disable").click
+      expect(page).to have_content("Item is no longer available for sale!")
+      @i_1.reload
+      expect(@i_1.enabled).to be_falsy
+    end
+  end
+end

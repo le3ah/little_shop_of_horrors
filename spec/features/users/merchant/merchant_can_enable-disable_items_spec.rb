285997f27d1d0e57350016e066f60b3e3a86d8af
stoic-plus <ricardoledesmadev@gmail.com>
2018-12-29 19:01:57 -0800

    adds sad path to dashboard_items if no image thumbnail, updates test for this

--- spec/features/users/merchant/merchant_can_enable-disable_items_spec.rb
+++ spec/features/users/merchant/merchant_can_enable-disable_items_spec.rb
@@ -4,8 +4,8 @@ describe "Merchant Enable/Disable Item" do
   context 'when merchant visits dashboard_items page' do
     before :each do
       @m = create(:user, role: 1)
-      @i_1 = create(:item, user: @m, enabled: true, thumbnail:"oldguy.jpeg")
-      @i_2 = create(:item, user: @m, thumbnail:"oldguy.jpeg")
+      @i_1 = create(:item, user: @m, enabled: true)
+      @i_2 = create(:item, user: @m)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m)
       visit dashboard_items_path

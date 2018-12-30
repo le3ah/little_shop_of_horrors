6fecbd93a26857bf30532826715fac91b4b45171
Tony Robertson <34175382+tonyr729@users.noreply.github.com>
2018-12-29 17:59:13 -0700

    Removes save_and_open_page

--- spec/features/users/merchant/merchant_destroy_spec.rb
+++ spec/features/users/merchant/merchant_destroy_spec.rb
@@ -7,12 +7,12 @@ describe "As a merchant" do
             item = create(:item, user:merchant, thumbnail:"oldguy.jpeg")
             allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
             visit dashboard_items_path
-            save_and_open_page
+            
             within "#item-#{item.id}" do 
                 expect(page).to have_button("delete")
                 click_button "delete"
             end 
-            save_and_open_page
+            
             expect(current_path).to eq(dashboard_items_path)
             expect(page).to have_content("item successfully deleted!") 
 
